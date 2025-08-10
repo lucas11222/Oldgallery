d6f67bc5708ea0de781586bace95e9909142e2ab
BuildTools <altamiranolucas2013@gmail.com>
2025-08-10 17:15:16 +0200

    Add server or computers.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_for, flash, g, Response
+from flask import Flask, render_template, request, session, redirect, url_for, flash, g, send_from_directory
 import flask_login, bcrypt, sqlite3, os, secrets
 from dotenv import load_dotenv
 load_dotenv()
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
+from werkzeug.utils import secure_filename
 
 # thanks csd4ni3l
 def get_db():
@@ -23,13 +24,28 @@ def get_db():
         """)
         db.execute("""
             CREATE TABLE IF NOT EXISTS Computers (
-                username TEXT NOT NULL,
-                computer_name TEXT PRIMARY KEY,
+                email TEXT NOT NULL,
+                server_name TEXT PRIMARY KEY,
+                description TEXT NOT NULL,
                 os TEXT NOT NULL,
                 ram INTEGER NOT NULL,
                 cpu TEXT NOT NULL,
                 gpu TEXT NOT NULL,
                 storage INTEGER NOT NULL
+            )
+        """)
+        db.execute("""
+            CREATE TABLE IF NOT EXISTS Servers (
+                email TEXT NOT NULL,
+                server_name TEXT PRIMARY KEY,
+                description TEXT NOT NULL,
+                os TEXT NOT NULL,
+                ram INTEGER NOT NULL,
+                cpu TEXT NOT NULL,
+                gpu TEXT NOT NULL,
+                storage INTEGER NOT NULL,
+                ssh INTEGER NOT NULL,
+                url INTEGER NOT NULL
             )
         """)
         db.commit()
@@ -55,6 +71,98 @@ def index():
     user_agent = request.user_agent
     logged_in = flask_login.current_user.is_authenticated
     return render_template('index.html', user_agent=user_agent, logged_in=logged_in)
+
+@app.route('/dashboard/computers')
+@flask_login.login_required
+def dashboard():
+    cur = get_db().cursor()
+    email = flask_login.current_user.id
+    cur.execute("SELECT computer_name, description, os, ram, cpu, gpu, storage FROM Computers WHERE email = ?", (email,))
+    rows = cur.fetchall()
+    return render_template('computers.html', rows=rows)
+
+@app.route('/dashboard/add_computer', methods=['POST'])
+@flask_login.login_required
+def add_computer():
+    data = request.form
+    computer_name = data.get('computer-name')
+    description = data.get('computer-description')
+    os = data.get('computer-os')
+    ram = data.get('computer-ram')
+    cpu = data.get('computer-cpu')
+    gpu = data.get('computer-gpu')
+    storage = data.get('computer-storage')
+    cur = get_db().cursor()
+    email = flask_login.current_user.id
+    cur.execute("SELECT 1 FROM Computers WHERE email = ? AND computer_name = ?", (email, computer_name))
+    if cur.fetchone():
+        flash("A computer with this name already exists :(", "error")
+        cur.close()
+        return redirect(url_for('computers'))
+    cur.execute("INSERT INTO Computers (email, computer_name, description, os, ram, cpu, gpu, storage) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (email, computer_name, description, os, ram, cpu, gpu, storage))
+    get_db().commit()
+    cur.close()
+    flash("computer added successfully :D", "success")
+    return redirect(url_for('computers'))
+
+@app.route('/dashboard/remove_computer', methods=['GET'])
+@flask_login.login_required
+def remove_computer():
+    cur = get_db().cursor()
+    computer_name = request.args.get('name')
+    email = flask_login.current_user.id
+    cur.execute("DELETE FROM Computers WHERE computer_name = ? AND email = ?",(computer_name, email))
+    get_db().commit()
+    cur.close()
+    flash("computer deleted successfully :(", "success")
+    return redirect(url_for('computers'))
+
+@app.route('/dashboard/servers')
+@flask_login.login_required
+def servers():
+    cur = get_db().cursor()
+    email = flask_login.current_user.id
+    cur.execute("SELECT server_name, description, os, ram, cpu, gpu, storage FROM Servers WHERE email = ?", (email,))
+    rows = cur.fetchall()
+    return render_template('servers.html', rows=rows)
+
+@app.route('/dashboard/add_Servers', methods=['POST'])
+@flask_login.login_required
+def add_servers():
+    data = request.form
+    server_name = data.get('Server-name')
+    description = data.get('Server-description')
+    os = data.get('Server-os')
+    ram = data.get('Server-ram')
+    cpu = data.get('Server-cpu')
+    gpu = data.get('Server-gpu')
+    storage = data.get('Server-storage')
+    ssh = data.get('Server-ssh')
+    url = data.get('Server-url')
+    cur = get_db().cursor()
+    email = flask_login.current_user.id
+    cur.execute("SELECT 1 FROM Server WHERE email = ? AND server_name = ?", (email, server_name))
+    if cur.fetchone():
+        flash("A computer with this name already exists :(", "error")
+        cur.close()
+        return redirect(url_for('servers'))
+    cur.execute("INSERT INTO Server (email, server_name, description, os, ram, cpu, gpu, storage, ssh, url) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (email, server_name, description, os, ram, cpu, gpu, storage, ssh, url))
+    get_db().commit()
+    cur.close()
+    flash("server added successfully :D", "success")
+    return redirect(url_for('servers'))
+
+@app.route('/dashboard/remove_Servers', methods=['GET'])
+@flask_login.login_required
+def remove_server():
+    cur = get_db().cursor()
+    server_name = request.args.get('name')
+    email = flask_login.current_user.id
+    cur.execute("DELETE FROM Servers WHERE server_name = ? AND email = ?",(server_name, email))
+    get_db().commit()
+    cur.close()
+    flash("server deleted successfully :(", "success")
+    return redirect(url_for('servers'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -93,6 +201,7 @@ def login():
         return redirect(url_for('index'))
     return render_template('login.html', user_agent=user_agent)
 
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
@@ -116,7 +225,7 @@ def signup():
         get_db().commit()
         cur.close()
         user = User()
-        user.id = username
+        user.id = email
         flask_login.login_user(user, remember=True)
         flash("signed up successfully :D", "success")
         return render_template('index.html', user_agent=user_agent)
